a3a5f1b886343ec063538ddd52e731d3240c2437
Lê Hoàng Phúc <58094077+maupro010@users.noreply.github.com>
2025-11-06 22:08:15 +0700

    Update addNew.py
    fix

--- addNew.py
+++ addNew.py
@@ -335,7 +335,7 @@ async def main():
             sh = gc.open_by_url(GOOGLE_SHEET_NAME)
             print("Kết nối thành công!")
             sheet_list = sh.worksheet("list")
-            # existing_stt_list = sheet_list.col_values(1)
+            existing_stt_list = sheet_list.col_values(1)
             list_novel = existing_stt_list[1:]
             ID = sheet_list.col_values(1)[-1]
             max_chapter = sheet_list.col_values(6)[-1]
